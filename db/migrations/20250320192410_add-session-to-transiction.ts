5614ed64d5338f2829d0a53e9a1add18539e03d9
CauaGNP <163431893+CauaGNP@users.noreply.github.com>
2025-03-25 03:36:29 +0000

    Adicionando o banco de dados

---
+++ db/migrations/20250320192410_add-session-to-transiction.ts
@@ -0,0 +1,20 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.alterTable("transictions", (table) => {
+        table.decimal("amount", 10,2).notNullable();
+        table.timestamp("create-at").defaultTo(knex.fn.now()).notNullable();
+        table.uuid("session_id").after("id").index();
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.alterTable( "transictions", (table) => {
+        table.dropColumn("session_id");
+        table.dropColumn("amount");
+        table.dropColumn("create-at");
+    });
+}
+
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,28 @@
 import fastify from "fastify"; 
-import { knexInstance } from "./database";
+// import { knexInstance } from "./database";
+import { env } from "./env";
+// import crypto from "node:crypto";
+
 
 const app = fastify();
 
 app.get("/users", async () => {
-    const tables = await knexInstance("sqlite_schema").select("*");
-    return tables;
+
+    // const transactions = await knexInstance("transictions").where("amount" , 1000).select("*");
+
+    // return transactions;
+
+    // const transactions = await knexInstance("transictions").insert({
+    //     id : crypto.randomUUID(),
+    //     title : "Transação teste",
+    //     amount : 1000,
+    // }).returning("*");
+
+    // return transactions
 });
 
 app.listen({
-    port : 3333
+    port : env.PORT,
 }).then(() => {
     console.log("Server Running!!");
 });
