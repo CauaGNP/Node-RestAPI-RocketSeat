13c8c40047a1e7bf11b86e499ebe04f42350a8c6
CauaGNP <cauagnp13@gmail.com>
2025-03-27 01:09:29 -0300

    adicionando rotas

--- src/routes/transitions.ts
+++ src/routes/transitions.ts
@@ -2,27 +2,41 @@ import { FastifyInstance } from "fastify";
 import { z } from "zod";
 import { randomUUID } from "node:crypto"
 import { knexInstance } from "../database"; 
+import { checkSessionExist } from "../middleware/check-session-id-exist";
+import { request } from "node:http";
 
 export async function transitionRoutes(app : FastifyInstance) {
-    app.get("/", async () => {
-        const transactions = await knexInstance("transictions").select();
+    app.get("/",{
+        preHandler : [checkSessionExist],
+    },async (request, reply ) => {
+        const { session_id } = request.cookies; 
+        const transactions = await knexInstance("transictions").where("session_id", session_id).select();
 
         return { transactions };
     });
 
-    app.get("/:id", async (request) => {
+    app.get("/:id",{
+        preHandler : [checkSessionExist]
+    },async (request) => {
         const getTransactionsParamsSchema = z.object({
             id : z.string().uuid(),
         })
 
         const { id } = getTransactionsParamsSchema.parse(request.params);
+        const { session_id } = request.cookies;
 
-        const transactions = await knexInstance("transictions").where("id", id).first();
+        const transactions = await knexInstance("transictions").where({
+            id,
+            "session_id" : session_id,
+        }).first();
         return { transactions };
     });
 
-    app.get("/summary", async ()=> {
-        const sumary = await knexInstance("transictions").sum("amount", { as : "amount" }).first();
+    app.get("/summary",{
+        preHandler : [checkSessionExist]
+    },async (request)=> {
+        const { session_id } = request.cookies;
+        const sumary = await knexInstance("transictions").where("session_id", session_id).sum("amount", { as : "amount" }).first();
 
         return { sumary };
     });
@@ -36,10 +50,21 @@ export async function transitionRoutes(app : FastifyInstance) {
 
         const { title, amount, type } = createTransacrionBodySchema.parse(request.body);
 
+        let sessionId = request.cookies.sessionId;
+
+        if(!sessionId){
+            sessionId = randomUUID();
+            reply.cookie("sessionId", sessionId, {
+                path : "/",
+                maxAge : 60 * 60 * 24 * 7, ///7 days
+            });
+        }
+
         await knexInstance("transictions").insert({
             id : randomUUID(),
             title,
             amount : type === "credit" ? amount : amount * -1,
+            session_id : sessionId,
         });
 
         return reply.code(201).send();
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import fastify from "fastify"; 
+import fastify from "fastify";
+import cookie from "@fastify/cookie"; 
 import { env } from "./env";
 import { transitionRoutes } from "./routes/transitions";
 
 const app = fastify();
 
+app.register(cookie);
 app.register(transitionRoutes, {
     prefix : "transactions"
 })
